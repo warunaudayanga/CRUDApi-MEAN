bf74fd04b33b3597ed3db82fc2f2c5ae63eeb648
warunaudayanga <warunaudayangalakruwan@gmail.com>
2021-05-20 20:11:37 +0530

    0.4

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bcrypt  =require('bcrypt');
+const bcrypt  =require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../modals/user');
 const authSecret = 'secret_code_change_later';
