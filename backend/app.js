c7ab7377e3ccf29e0a516ebd7e8cbd33f418c42a
warunaudayanga <warunaudayangalakruwan@gmail.com>
2021-05-20 14:19:42 +0530

    0.3 (authentication completed) --> to Heroku

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ const dbUrl = {local: 'mongodb://localhost:27017/db', cloud: 'mongodb+srv://admi
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
-mongoose.connect(dbUrl.local, { // <-- local
+mongoose.connect(dbUrl.cloud, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => {
